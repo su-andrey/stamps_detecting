b334e08a9145386886980248a4110af72ef2a163
Andrei <shvyrev.a.a@mail.ru>
2025-07-14 20:19:38 +0300

    new structure

---
+++ app/api/main.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from app.model.model import YOLOStamp, yolo_pipeline
+import torch
+
+app = FastAPI()
+model = YOLOStamp() # Инициализируем модель и загружаем в неё веса
+model.load_state_dict(torch.load("app/api/weight.pt"))
+@app.post("/detect/")
+async def get_boxes(file: UploadFile = File(...)):
+    image = Image.open(file.file)
+    result = yolo_pipeline(model, "cpu", image)  # Можно указать необходимый тип устройства
+    return JSONResponse(content={"detections": result.numpy().tolist()})
