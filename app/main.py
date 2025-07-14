d8a017e09983edd07958c2783739048770002acd
Andrei <shvyrev.a.a@mail.ru>
2025-07-14 18:29:08 +0300

    strucuture update

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from model_pipelane import YoloStampPipeline
+from PIL import Image
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+app = FastAPI()
+
+
+@app.post("/detect/")
+async def get_boxes(file: UploadFile = File(...)):
+    pipeline = YoloStampPipeline.from_pretrained("stamps-labs/yolo-stamp", "cpu")
+    print(file.file)
+    image = Image.open(file.file)
+    result = pipeline(image)
+    return JSONResponse(content={"detections": result.numpy().tolist()})
